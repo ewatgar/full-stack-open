5adfb029f37b81d19bb5f2245a1cc9142234bc1d
ender <103525727+ewatgar@users.noreply.github.com>
2024-08-24 21:54:26 +0200

    add html template and basic state unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react"
+
 const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleOnClick = (event) => {
+        switch (event.target.id) {
+            case 'good':
+                setGood(good + 1)
+                break;
+            case 'neutral':
+                setNeutral(neutral + 1)
+                break;
+            case 'bad':
+                setBad(bad + 1)
+                break;
+        }
+    }
+
     return (
         <>
-            <h1>hello world</h1>
+            <h1>give feedback</h1>
+            <div>
+                <button onClick={handleOnClick} id="good">good</button>
+                <button onClick={handleOnClick} id="neutral">neutral</button>
+                <button onClick={handleOnClick} id="bad">bad</button>
+            </div>
+            <h1>statistics</h1>
+            <p>
+                good {good}
+                <br />
+                neutral {neutral}
+                <br />
+                bad {bad}
+            </p>
         </>
     )
 }
