c1bce351e73c8ab82ab19b25e7af59159d5097c4
ender <103525727+ewatgar@users.noreply.github.com>
2024-08-24 22:28:22 +0200

    unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react"
 
+const Statistics = ({ good, neutral, bad }) => {
+    const all = good + neutral + bad;
+    const average = (good - bad) != 0 ? (good - bad) / all : 0;
+    const positive = good != 0 ? good / all * 100 : 0;
+
+    return (
+        <>
+            <h1>statistics</h1>
+            <div id="stats-list">
+                <div>good {good}</div>
+                <div>neutral {neutral}</div>
+                <div>bad {bad}</div>
+                <div>all {all}</div>
+                <div>average {average}</div>
+                <div>positive {positive} %</div>
+            </div>
+        </>
+    )
+}
+
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    const all = good + neutral + bad;
-    const average = (good - bad) / all;
-    const positive = good != 0 ? good / all * 100 : 0;
+
 
     const handleOnClick = (event) => {
         switch (event.target.id) {
@@ -30,15 +48,10 @@ const App = () => {
                 <button onClick={handleOnClick} id="neutral">neutral</button>
                 <button onClick={handleOnClick} id="bad">bad</button>
             </div>
-            <h1>statistics</h1>
-            <div id="stats-list">
-                <div>good {good}</div>
-                <div>neutral {neutral}</div>
-                <div>bad {bad}</div>
-                <div>all {all}</div>
-                <div>average {average}</div>
-                <div>positive {positive} %</div>
-            </div>
+            <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad} />
         </>
     )
 }
