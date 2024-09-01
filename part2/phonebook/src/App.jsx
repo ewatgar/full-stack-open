f394a2af75b65d72aa1cfa7fda0afe3380094bb3
ender <103525727+ewatgar@users.noreply.github.com>
2024-09-01 23:51:28 +0200

    wip phonebook step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,10 +1,37 @@
+import { useState } from 'react';
+
 const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
+  const handleOnSubmitForm = (event) => {
+    event.preventDefault();
+    if (persons.some((p) => p.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      const newPerson = { name: newName };
+      setPersons([...persons, newPerson]);
+    }
+    setNewName('');
+  };
+
+  const handleOnChangeInput = (event) => setNewName(event.target.value);
 
   return (
-    <>
-      <h1>hello world</h1>
-    </>
-  )
-}
+    <div>
+      <div>debug name: {newName}</div>
+      <div>debug number: {newName}</div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleOnSubmitForm}>
+        <div>name: <input onChange={handleOnChangeInput} value={newName} /></div>
+        <div>number: <input /></div>
+        <div><button type="submit">add</button></div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((p) => <div key={p.name}>{p.name}</div>)}
+    </div>
+  );
+};
 
-export default App
+export default App;
